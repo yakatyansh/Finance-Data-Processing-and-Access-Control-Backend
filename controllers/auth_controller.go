8c6507084c96938b3571ff4b1146961472a4c4ee
yakatyansh <ykpetrolhead08@outlook.com>
2026-04-02 12:12:08 +0530

    base code done

---
+++ controllers/auth_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"finance-backend/models"
+	"finance-backend/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	createdUser, err := services.RegisterUser(user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, createdUser)
+}
---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"finance-backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine) {
+	auth := r.Group("/auth")
+	{
+		auth.POST("/register", controllers.Register)
+	}
+}
